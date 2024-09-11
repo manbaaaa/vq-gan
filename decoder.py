7342369d5100c58a630f808bc8af40358766777f
lishaojie <shaojieli.nlp@gmail.com>
2024-09-11 08:52:23 +0000

    decoder

--- decoder.py
+++ decoder.py
@@ -25,36 +25,37 @@ from helper import (
 )
 
 
-class Encoder(nn.Module):
-    def __init__(self, args):
+class Decoder(nn.Module):
+    def __init__():
         super().__init__()
-        channels = [128, 128, 128, 256, 256, 512]
+        channels = [512, 256, 256, 128, 128]
         attn_resolution = [16]
-        num_res_blocks = 2
-        resolution = 256
+        num_res_blocks = 3
+        resolution = 16
+        in_channels = channels[0]
         layers = [
-            nn.Conv2d(
-                args.image_channels, channels[0], kernel_size=3, stride=1, padding=1
-            )
+            nn.Conv2d(args.latent_dim, in_channels, kernel_size=3, stride=1, padding=1),
+            ResidualBlock(in_channels, in_channels),
+            NonLocalBlock(in_channels),
+            ResidualBlock(in_channels, in_channels),
         ]
-        for i in range(len(channels) - 1):
-            in_channels = channels[i]
-            out_channels = channels[i + 1]
+        for i in range(len(channels)):
+            out_channels = channels[i]
             for _ in range(num_res_blocks):
                 layers.append(ResidualBlock(in_channels, out_channels))
                 in_channels = out_channels
                 if resolution in attn_resolution:
                     layers.append(NonLocalBlock(in_channels))
-            if i != len(channels) - 2:
-                layers.append(DownSampleBlock(in_channels, out_channels))
-                resolution = resolution // 2
-        layers.append(ResidualBlock(channels[-1], channels[-1]))
-        layers.append(NonLocalBlock(channels[-1]))
-        layers.append(ResidualBlock(channels[-1], channels[-1]))
+            if i != 0:
+                layers.append(UpSampleBlock(in_channels, out_channels))
+                resolution = resolution * 2
+
         layers.append(GroupNorm(channels[-1]))
         layers.append(Swish())
         layers.append(
-            nn.Conv2d(channels[-1], args.latent_dim, kernel_size=3, stride=1, padding=1)
+            nn.Conv2d(
+                channels[-1], args.image_channels, kernel_size=3, stride=1, padding=1
+            )
         )
         self.layers = nn.Sequential(*layers)
 
