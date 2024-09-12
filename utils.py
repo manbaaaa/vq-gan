5b1b5ae5c5018399aa05be8291980257e4a3f815
lishaojie <shaojieli.nlp@gmail.com>
2024-09-12 08:00:37 +0000

    utils

---
+++ utils.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# Copyright (c) 2024 Shaojie Li (shaojieli.nlp@gmail.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+import albumentations
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn as nn
+from PIL import Image
+from torch.utils.data import DataLoader, Dataset
+
+# --------------------------------------------- #
+#                  Data Utils
+# --------------------------------------------- #
+
+
+class ImagePaths(Dataset):
+    def __init__(self, path, size=None):
+        self.size = size
+
+        self.images = [os.path.join(path, file) for file in os.listdir(path)]
+        self._length = len(self.images)
+
+        self.rescaler = albumentations.SmallestMaxSize(max_size=self.size)
+        self.cropper = albumentations.CenterCrop(height=self.size, width=self.size)
+        self.preprocessor = albumentations.Compose([self.rescaler, self.cropper])
+
+    def __len__(self):
+        return self._length
+
+    def preprocess_image(self, image_path):
+        image = Image.open(image_path)
+        if not image.mode == "RGB":
+            image = image.convert("RGB")
+        image = np.array(image).astype(np.uint8)
+        image = self.preprocessor(image=image)["image"]
+        image = (image / 127.5 - 1.0).astype(np.float32)
+        image = image.transpose(2, 0, 1)
+        return image
+
+    def __getitem__(self, i):
+        example = self.preprocess_image(self.images[i])
+        return example
+
+
+def load_data(args):
+    train_data = ImagePaths(args.dataset_path, size=256)
+    train_loader = DataLoader(train_data, batch_size=args.batch_size, shuffle=False)
+    return train_loader
+
+
+# --------------------------------------------- #
+#                  Module Utils
+#            for Encoder, Decoder etc.
+# --------------------------------------------- #
+
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find("Conv") != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find("BatchNorm") != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+
+
+def plot_images(images):
+    x = images["input"]
+    reconstruction = images["rec"]
+    half_sample = images["half_sample"]
+    full_sample = images["full_sample"]
+
+    fig, axarr = plt.subplots(1, 4)
+    axarr[0].imshow(x.cpu().detach().numpy()[0].transpose(1, 2, 0))
+    axarr[1].imshow(reconstruction.cpu().detach().numpy()[0].transpose(1, 2, 0))
+    axarr[2].imshow(half_sample.cpu().detach().numpy()[0].transpose(1, 2, 0))
+    axarr[3].imshow(full_sample.cpu().detach().numpy()[0].transpose(1, 2, 0))
+    plt.show()
